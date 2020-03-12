dca2ac9d798b2fe2bc4891d75099663c5001278f
juanmanuelgodoy <juanmanuelgodoy@yahoo.com.ar>
2020-03-12 11:27:58 -0300

    creacion de la segunda entidad

---
+++ src/main/java/pruebaqi/entidades/Mascota.java
@@ -0,0 +1,38 @@
+package pruebaqi.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Mascota {
+
+	@Id
+	private String id;
+	private String nombre;
+	private String raza;
+	public Mascota(String id, String nombre, String raza) {
+		this.id = id;
+		this.nombre = nombre;
+		this.raza = raza;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	
+	
+}
