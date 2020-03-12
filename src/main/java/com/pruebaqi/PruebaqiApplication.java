1c8541d3fac0fd28735ce5297a486e4809e0b214
juanmanuelgodoy <juanmanuelgodoy@yahoo.com.ar>
2020-03-12 10:49:46 -0300

    Primer commit

---
+++ src/main/java/com/pruebaqi/PruebaqiApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaqi/PruebaqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
