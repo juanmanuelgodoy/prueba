aed8a026dfc7a83afbc82d1ca4a953376f772baf
juanmanuelgodoy <juanmanuelgodoy@yahoo.com.ar>
2020-03-12 11:12:17 -0300

    Creacion de la primera entidad

---
+++ src/main/java/pruebaqi/entidades/Persona.java
@@ -0,0 +1,38 @@
+package pruebaqi.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Persona {
+
+	@Id
+	private String id;
+	private String nombre;
+	private String apellido;
+	public Persona(String id, String nombre, String apellido) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+}
