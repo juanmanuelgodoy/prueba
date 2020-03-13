666c22bc9e6fe7f0a5e79adfeaad51ae57b35642
juanmanuelgodoy <juanmanuelgodoy@yahoo.com.ar>
2020-03-12 23:31:33 -0300

    Creacion de personaservicio

---
+++ src/main/java/pruebaqi/servicios/PersonaServicio.java
@@ -0,0 +1,14 @@
+package pruebaqi.servicios;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaServicio {
+
+	@Transactional
+    public void eliminarPersona(String id) {
+}
+}
+
