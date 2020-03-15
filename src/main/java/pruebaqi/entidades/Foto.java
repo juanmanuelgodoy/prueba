fa48e34188e885f9eff64b8511715b1436d1a32d
juanmanuelgodoy <juanmanuelgodoy@yahoo.com.ar>
2020-03-14 23:43:08 -0300

    agregamos el constructor con herencia en mascota

--- src/main/java/pruebaqi/entidades/Foto.java
+++ src/main/java/pruebaqi/entidades/Foto.java
@@ -10,6 +10,13 @@ public class Foto {
 	private String id;
 	private String nombre;
 	private String apellido;
+		
+	public Foto(String id, String nombre, String apellido) {
+	
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
 	public String getId() {
 		return id;
 	}
