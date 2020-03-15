f6c17a1323138ed717eaa351d1c925adeb8cb4ab
juanmanuelgodoy <juanmanuelgodoy@yahoo.com.ar>
2020-03-14 21:10:04 -0300

    agregamos el codigo de la entidad foto

--- src/main/java/pruebaqi/entidades/Foto.java
+++ src/main/java/pruebaqi/entidades/Foto.java
@@ -1,5 +1,33 @@
 package pruebaqi.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Foto {
 
+	@Id
+	private String id;
+	private String nombre;
+	private String apellido;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
 }
