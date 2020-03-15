c149b73bf4920f1a8ad1eb06f610e1dd744bf930
juanmanuelgodoy <juanmanuelgodoy@yahoo.com.ar>
2020-03-14 21:14:07 -0300

    agregamos el to string en entidad foto

--- src/main/java/pruebaqi/entidades/Foto.java
+++ src/main/java/pruebaqi/entidades/Foto.java
@@ -1,5 +1,13 @@
 package pruebaqi.entidades;
 
+
 public class Foto {
 
+	
+	@Override
+	public String toString() {
+		return "Foto [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+	}
+	
+	
 }
